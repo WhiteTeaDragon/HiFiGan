33f4560b92f085139475642d3a74ccedbf118932
asendero <AlexSend57@gmail.com>
2021-12-16 22:50:08 +0300

    more for loops

--- hifigan/loss/HiFiLoss.py
+++ hifigan/loss/HiFiLoss.py
@@ -14,10 +14,16 @@ class HiFiLoss(torch.nn.Module):
         return loss
 
     def real_loss(self, target_res):
-        return torch.sum(torch.mean((1 - target_res) ** 2, dim=0))
+        loss = 0
+        for layer in target_res:
+            loss += torch.mean((1 - layer) ** 2)
+        return loss
 
     def fake_loss(self, model_res):
-        torch.sum(torch.mean(model_res ** 2, dim=0))
+        loss = 0
+        for layer in model_res:
+            loss += torch.mean(layer ** 2)
+        return loss
 
     def disc_forward(self, target_res, model_res):
         return self.real_loss(target_res) + self.fake_loss(model_res)
