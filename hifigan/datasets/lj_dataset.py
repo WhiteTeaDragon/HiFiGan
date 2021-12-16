bdfea066fbdbce0cb3afedd9ad2c845e87cdf779
asendero <AlexSend57@gmail.com>
2021-12-16 15:33:16 +0300

    add segments

--- hifigan/datasets/lj_dataset.py
+++ hifigan/datasets/lj_dataset.py
@@ -1,4 +1,6 @@
 import logging
+import random
+
 import torchaudio
 import torch
 import librosa
@@ -10,17 +12,26 @@ logger = logging.getLogger(__name__)
 
 
 class LJSpeechDataset(torchaudio.datasets.LJSPEECH):
-    def __init__(self, config_parser, data_dir=None, download="True"):
+    def __init__(self, config_parser, segment_size, data_dir=None,
+                 download="True"):
         if data_dir is None:
             data_dir = ROOT_PATH / "data" / "datasets" / "lj"
             data_dir.mkdir(exist_ok=True, parents=True)
         super().__init__(root=data_dir, download=(download == "True"))
         self.config_parser = config_parser
         self.wave2spec = initialize_mel_spec(self.config_parser)
+        self.segment_size = segment_size
 
     def __getitem__(self, index: int):
         waveform, _, _, _ = super().__getitem__(index)
         waveform_length = torch.tensor([waveform.shape[-1]]).int()
+        if waveform_length > self.segment_size:
+            max_audio_start = waveform_length - self.segment_size
+            audio_start = random.randint(0, max_audio_start)
+            waveform = waveform[:, audio_start:audio_start + self.segment_size]
+        else:
+            waveform = torch.nn.functional.pad(
+                waveform, (0, self.segment_size - waveform_length), 'constant')
 
         melspec, _ = self.get_spectrogram(waveform)
         melspec_length = torch.tensor([melspec.shape[-1]]).int()
