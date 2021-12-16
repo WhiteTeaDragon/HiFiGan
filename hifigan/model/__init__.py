3ec9293af59e963eaab7cfbf40289893d9007da2
asendero <AlexSend57@gmail.com>
2021-12-16 11:57:07 +0300

    set many epochs

--- hifigan/model/__init__.py
+++ hifigan/model/__init__.py
@@ -1,5 +1,5 @@
-from pix2pix.model.generator import Generator
-from pix2pix.model.discriminator import Discriminator
+from hifigan.model.generator import Generator
+from hifigan.model.discriminator import Discriminator
 
 __all__ = [
     "Generator",
